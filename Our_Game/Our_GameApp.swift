9c44ae703d1d10bf05da291d02ffba8b6fc22ab9
joudy003 <joudy.j.t@gmail.com>
2025-05-01 22:59:23 +0300

    Initial Commit

---
+++ Our_Game/Our_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Our_GameApp.swift
+//  Our_Game
+//
+//  Created by Joud Abdullah AlAmri on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Our_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Our_GameTests/Our_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Our_GameTests.swift
+//  Our_GameTests
+//
+//  Created by Joud Abdullah AlAmri on 5/1/25.
+//
+
+import Testing
+@testable import Our_Game
+
+struct Our_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
