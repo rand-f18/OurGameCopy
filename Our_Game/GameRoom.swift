f2516050ea2c1c20580aa07b1d66ff73e6ee309b
Albatoul <ebatoul25@gmail.com>
2025-05-05 17:22:04 +0300

    Adding GameRoom

---
+++ Our_Game/GameRoom.swift
@@ -0,0 +1,18 @@
+//
+//  GameRoom.swift
+//  Our_Game
+//
+//  Created by Albatoul Alhamas on 07/11/1446 AH.
+//
+
+import SwiftUI
+
+struct GameRoom: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    GameRoom()
+}
