77c02117de2c0b53913a877f5957a3b380777855
Rand <randalobaid9@gmail.com>
2025-05-08 09:12:59 +0300

    StartPrivateRoom

---
+++ Our_Game/StartPrivateRoom.swift
@@ -0,0 +1,18 @@
+//
+//  StartPrivateRoom.swift
+//  Our_Game
+//
+//  Created by Rand Soliman Alobaid on 10/11/1446 AH.
+//
+
+import SwiftUI
+
+struct StartPrivateRoom: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartPrivateRoom()
+}
